6d382e93db1c8a741505dffba6a023c3e3391b28
ohbarye <over.rye@gmail.com>
2015-05-26 21:26:14 +0900

    ready for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --env DJANGO_SETTINGS_MODULE=mysite.settings mysite.wsgi --log-file -
--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(
     install_requires=[
         'django',
         'django-enumfield',
+        'gunicorn',
+        'whitenoise',
     ],
 )
