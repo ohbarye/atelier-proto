4d2c69b3a3d8a5a36deba2049463dfb985690f8f
ohbarye <over.rye@gmail.com>
2015-05-26 22:22:00 +0900

    retry heroku deploy. modify staticfiles settind

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -111,11 +111,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFIELS_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
